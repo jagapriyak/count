764173f24e1d7c458766df14e1fffaed4fe61fd6
jagapriyak <jagapriyak25@gmail.com>
2025-05-16 12:00:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log(window)
+console.log(document)
+setTimeout(
+    ()=>{
+    document.getElementById("count").innerHTML=10
+    setTimeout(()=>{
+        document.getElementById("count").innerHTML=9
+        setTimeout(()=>{
+             document.getElementById("count").innerHTML=8
+             setTimeout(()=>{
+                 document.getElementById("count").innerHTML=7
+                 setTimeout(() => {
+                     document.getElementById("count").innerHTML=6 
+                    setTimeout(() => {
+                        document.getElementById("count").innerHTML=5
+                       setTimeout(() => {
+                           document.getElementById("count").innerHTML=4 
+                           setTimeout(() => {
+                                document.getElementById("count").innerHTML=3
+                            setTimeout(() => {
+                                document.getElementById("count").innerHTML=2 
+                                setTimeout(() => {
+                                    document.getElementById("count").innerHTML=1
+                                    setTimeout(() => {
+                                        document.getElementById("count").innerHTML=0 
+                                        setTimeout(() => {
+                                            document.getElementById("count").innerHTML= "HAPPY BIRTHDAY TO ME🥳...:)🎉"
+                                            setTimeout(()=>{
+
+                                                },1000);
+                                            }, 1000);
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                 }, 1000);
+             },1000)
+        },1000)
+    },1000)
+ },1000 
+)
